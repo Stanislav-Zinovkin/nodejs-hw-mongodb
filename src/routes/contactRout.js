1201830c6ce3d8c16b8dcf050ce9fd1af65f7e12
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2025-07-25 12:13:23 +0300

    validation\v0.5

--- src/routes/contactRout.js
+++ src/routes/contactRout.js
@@ -7,8 +7,8 @@ import { invalidFormatId } from '../middlewares/invalidFormatId.js';
 const router = express.Router();
 
 router.get('/', ctrlWrapper(handleGetAllContacts));
-router.get('/:contactId', invalidFormatId,ctrlWrapper(handleGetContactById));
+router.get('/:contactId', invalidFormatId('contactId'),ctrlWrapper(handleGetContactById));
 router.post('/', validateBody(createContactSchema),ctrlWrapper(handleCreateContact));
-router.patch('/:contactId', invalidFormatId, validateBody(updateContactSchema),ctrlWrapper(handleUpdateContact));
-router.delete('/:contactId', invalidFormatId, ctrlWrapper(handleDeleteContact));
+router.patch('/:contactId', invalidFormatId('contactId'), validateBody(updateContactSchema),ctrlWrapper(handleUpdateContact));
+router.delete('/:contactId', invalidFormatId('contactId'), ctrlWrapper(handleDeleteContact));
 export default router;
