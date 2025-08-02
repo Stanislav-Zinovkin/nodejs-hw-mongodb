e03041210fb6274ac30dee8ef0c3c3689a461c86
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2025-08-02 13:28:09 +0300

    auth/v1.0

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import 'dotenv/config';
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
+console.log('ACCESS_TOKEN_SECRET:', process.env.ACCESS_TOKEN_SECRET);
+console.log('REFRESH_TOKEN_SECRET:', process.env.REFRESH_TOKEN_SECRET);
+
 async function startApp()  {
     await initMongoConnection();
     setupServer();
