5640440e853adb1ef4ea5491e90303623c7f1036
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2025-08-11 17:43:52 +0300

    images/v0.3

--- src/utils/saveFileToUploadDir.js
+++ src/utils/saveFileToUploadDir.js
@@ -1,12 +1,12 @@
 import path from 'path';
 import fs from 'node:fs/promises';
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from '../index.js';
-import { getEnvVar} from './getEnvVar.js';
+
 
 export const saveFileToUploadDir = async (file) => {
     await fs.rename(
         path.join(TEMP_UPLOAD_DIR, file.filename),
         path.join(UPLOAD_DIR, file.filename),
     );
-    return `${getEnvVar('APP_DOMAIN')}/uploads/${file.filename}`;
+    return `${process.env.APP_DOMAIN}/uploads/${file.filename}`;
 }
