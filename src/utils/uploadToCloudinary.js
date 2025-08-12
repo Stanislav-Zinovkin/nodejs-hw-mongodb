2d8f0a491fb57651fe46e1d6c20202285619e1a5
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2025-08-12 10:15:42 +0300

    images/v1.3

--- src/utils/uploadToCloudinary.js
+++ src/utils/uploadToCloudinary.js
@@ -6,7 +6,7 @@ import cloudinary from './cloudinary.js';
 dotenv.config();
 
 export const uploadToCloudinary = async (file) => {
- try{ const response = await cloudinary.v2.uploader.upload(file.path);
+ try{ const response = await cloudinary.uploader.upload(file.path);
   await fs.unlink(file.path);
   return response.secure_url;} catch(error){
     console.log('cloudinary upload error:', error);
