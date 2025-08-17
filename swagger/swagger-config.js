3cdce8747d0765671fa022b871ed91cfad931325
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2025-08-17 22:19:56 +0300

    swagger/v0.8

--- swagger/swagger-config.js
+++ swagger/swagger-config.js
@@ -1,26 +1,17 @@
-import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
-
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Phonebook API",
-      version: "1.0.0",
-      description: "API documentation for the Phonebook project",
-    },
-    servers: [
-      {
-        url: "https://nodejs-hw-mongodb-contacts-i3qb.onrender.com",
-      },
-    ],
-  },
-  apis: ["./swagger/**/*.yaml"], 
-
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+import fs from "fs";
+import path from "path";
 
 export const swaggerDocs = (app) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+ 
+  const swaggerFilePath = path.join(process.cwd(), "swagger", "openapi.yaml");
+
+  
+  const yaml = fs.readFileSync(swaggerFilePath, "utf8");
+  const swaggerDocument = require("js-yaml").load(yaml);
+
+ 
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+  console.log("Swagger docs available at http://localhost:3000/api-docs");
 };
